3aa5e0d96572895a522b13a345cce636e47a4931
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-02 09:39:37 -0600

    reformateado base de codigo completo

--- ecommerce/src/main/java/com/stellaTech/ecommerce/repository/PlatformUserIdentity.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/repository/PlatformUserIdentity.java
@@ -1,9 +1,8 @@
 package com.stellaTech.ecommerce.repository;
 
+import com.stellaTech.ecommerce.model.PlatformUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.stellaTech.ecommerce.model.PlatformUser;
 
 @Repository
 public interface PlatformUserIdentity extends JpaRepository<PlatformUser, Long> {
