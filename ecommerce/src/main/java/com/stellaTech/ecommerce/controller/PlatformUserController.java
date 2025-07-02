d646397050057ccd78740f6f412de4d8d83b437b
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-02 08:36:02 -0600

    implementado get y post de usuario

--- ecommerce/src/main/java/com/stellaTech/ecommerce/controller/PlatformUserController.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/controller/PlatformUserController.java
@@ -1,4 +1,26 @@
 package com.stellaTech.ecommerce.controller;
 
+import com.stellaTech.ecommerce.model.PlatformUser;
+import com.stellaTech.ecommerce.repository.PlatformUserIdentity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
 public class PlatformUserController {
+    @Autowired
+    private PlatformUserIdentity userIdentity;
+
+    @GetMapping("/users")
+    public List<PlatformUser> getAllUsers() {
+        return userIdentity.findAll();
+    }
+
+    @PostMapping("/users")
+    public PlatformUser createUser(@RequestBody PlatformUser newUser) {
+        return userIdentity.save(newUser);
+    }
 }
