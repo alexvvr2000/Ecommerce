06dc730a770f16cb73e815c29b9192a7c094ad5d
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-03 15:39:57 -0600

    agregado jpa executor a PlatformUserIdentity

--- ecommerce/src/main/java/com/stellaTech/ecommerce/repository/PlatformUserIdentity.java
+++
@@ -1,9 +0,0 @@
-package com.stellaTech.ecommerce.repository;
-
-import com.stellaTech.ecommerce.model.PlatformUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlatformUserIdentity extends JpaRepository<PlatformUser, Long> {
-}
---
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/repository/PlatformUserIdentity.java
@@ -0,0 +1,10 @@
+package com.stellaTech.ecommerce.service.repository;
+
+import com.stellaTech.ecommerce.model.PlatformUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlatformUserIdentity extends JpaRepository<PlatformUser, Long>, JpaSpecificationExecutor<PlatformUser> {
+}
