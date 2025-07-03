2f9ac4ef18b912378413a2aa5998e1b71d2f2334
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-03 15:45:28 -0600

    agregada entidad Product base

---
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/model/Product.java
@@ -0,0 +1,9 @@
+package com.stellaTech.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product", schema = "product_data")
+public class Product {
+}
