aeabd64fe7e7e2175da10fbc6a2a297221269e28
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-03 19:56:42 -0600

    implementados metodos para lectura de ProductController

--- ecommerce/src/main/java/com/stellaTech/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/controller/ProductController.java
@@ -1,11 +1,26 @@
 package com.stellaTech.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stellaTech.ecommerce.model.Product;
+import com.stellaTech.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllActiveProducts();
+    }
+
+    @GetMapping("/products/{idProduct}")
+    public Product getProduct(@PathVariable Long idProduct) {
+        return productService.getProductById(idProduct);
+    }
 }
