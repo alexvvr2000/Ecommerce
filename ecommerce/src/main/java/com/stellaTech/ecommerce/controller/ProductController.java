44189e9fa96499df8cfbc15ca99441c1519d606b
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-03 20:00:55 -0600

    implementados metodos de patch y put de ProductController

--- ecommerce/src/main/java/com/stellaTech/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.stellaTech.ecommerce.controller;
 
+import com.stellaTech.ecommerce.model.PlatformUser;
 import com.stellaTech.ecommerce.model.Product;
 import com.stellaTech.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -22,5 +25,23 @@ public class ProductController {
     @GetMapping("/products/{idProduct}")
     public Product getProduct(@PathVariable Long idProduct) {
         return productService.getProductById(idProduct);
+    }
+
+    @PutMapping("/products/{idProduct}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Long idProduct,
+            @RequestBody Product updatedProduct
+    ) {
+        Product savedProduct = productService.updateEntireProduct(idProduct, updatedProduct);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @PatchMapping("/users/{idProduct}")
+    public ResponseEntity<Product> partialUpdateUser(
+            @PathVariable Long idProduct,
+            @RequestBody Map<String, Object> updatedFields
+    ) {
+        Product savedProduct = productService.updateProductPartially(idProduct, updatedFields);
+        return ResponseEntity.ok(savedProduct);
     }
 }
