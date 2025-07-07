6465ad1d6a7f65f0277e2a58b0738a138dbf5924
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-07 10:04:08 -0600

    OrderRepository ahora tiene declaracion correcta

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/repository/OrderRepository.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<OrderPK, Long>, JpaSpecificationExecutor<Order> {
+public interface OrderRepository extends JpaRepository<Order, OrderPK>, JpaSpecificationExecutor<Order> {
 }
