6141338ca298b03b85db79472b2094ab34d2d338
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-06 23:41:22 -0600

    cambiado specs de Order para usar llave compuesta

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/specification/OrderSpecs.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/specification/OrderSpecs.java
@@ -1,6 +1,7 @@
 package com.stellaTech.ecommerce.service.specification;
 
 import com.stellaTech.ecommerce.model.Order;
+import com.stellaTech.ecommerce.model.OrderPK;
 import org.springframework.data.jpa.domain.Specification;
 
 public class OrderSpecs {
@@ -8,9 +9,10 @@ public class OrderSpecs {
         return (root, query, cb) -> cb.equal(root.get("deleted"), false);
     }
 
-    public static Specification<Order> activeProductById(Long id) {
-        return isNotDeleted().and(
-                (root, query, cb) -> cb.equal(root.get("id"), id)
+    public static Specification<Order> hasId(OrderPK id) {
+        return (root, query, cb) -> cb.and(
+                cb.equal(root.get("id").get("productId"), id.getProductId()),
+                cb.equal(root.get("id").get("userId"), id.getUserId())
         );
     }
 }
