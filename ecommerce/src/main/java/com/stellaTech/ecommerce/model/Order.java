d6e6046f3f403fb8c80d6e366130ee5196ad73e5
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-07 11:04:34 -0600

    agregado campo id con identity

--- ecommerce/src/main/java/com/stellaTech/ecommerce/model/Order.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/model/Order.java
@@ -10,6 +10,9 @@ import java.util.Date;
 @Table(name = "order", schema = "product_data")
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @EmbeddedId
     @AttributeOverrides(
             {
@@ -18,32 +21,31 @@ public class Order {
             }
     )
     private OrderPK orderPK;
-
     @MapsId("productId")
     @ManyToOne
     private Product product;
-
     @MapsId("userId")
     @ManyToOne
     private PlatformUser platformUser;
-
     @Column(name = "purchased_date", updatable = false)
     @CreationTimestamp
     private Date purchasedDate;
-
     @Column(name = "deleted", nullable = false)
     @JsonIgnore
     private boolean deleted = false;
-
     @Column(name = "product_count", nullable = false, updatable = false)
     private int productCount = 1;
 
     public Order() {
     }
 
-    public Order(OrderPK orderPK, int productCount) throws Exception{
+    public Order(OrderPK orderPK, int productCount) throws Exception {
         this.setOrderPK(orderPK);
         this.setProductCount(productCount);
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public PlatformUser getPlatformUser() {
@@ -62,8 +64,8 @@ public class Order {
         return productCount;
     }
 
-    public void setProductCount(int productCount) throws Exception{
-        if(productCount < 0){
+    public void setProductCount(int productCount) throws Exception {
+        if (productCount < 0) {
             throw new Exception("The product count for the order is not valid");
         }
         this.productCount = productCount;
