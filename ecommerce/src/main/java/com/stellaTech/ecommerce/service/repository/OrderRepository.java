e04b9fc07386b29b5d10c26a7e3f61ef167f9099
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-06 23:32:17 -0600

    OrderRepository ahora usa OrderPK como le corresponde

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/repository/OrderRepository.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/repository/OrderRepository.java
@@ -1,10 +1,11 @@
 package com.stellaTech.ecommerce.service.repository;
 
 import com.stellaTech.ecommerce.model.Order;
+import com.stellaTech.ecommerce.model.OrderPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+public interface OrderRepository extends JpaRepository<OrderPK, Long>, JpaSpecificationExecutor<Order> {
 }
