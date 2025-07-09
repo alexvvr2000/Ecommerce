9c820448d66f517922567491a64e9b6a437f7dd3
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-09 17:50:29 -0600

    ahora PlatformUserMapper ignora nulls en update

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/platformUser/PlatformUserMapper.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/platformUser/PlatformUserMapper.java
@@ -1,15 +1,14 @@
 package com.stellaTech.ecommerce.service.dto.platformUser;
 
 import com.stellaTech.ecommerce.model.PlatformUser;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public abstract class PlatformUserMapper {
     @Mapping(target = "deleted", ignore = true)
     public abstract PlatformUser toEntity(PlatformUserInsertDto platformUserInsertDto);
 
+    @BeanMapping(nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "deleted", ignore = true)
     public abstract PlatformUser updatePlatformUserFromDto(@MappingTarget PlatformUser entity, PlatformUserPatchDto dto);
 
