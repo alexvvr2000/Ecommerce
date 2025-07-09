c1b7221f3706262314a0143e8d027728906b2abc
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-09 15:20:44 -0600

    implementados hash equals a DTO's

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/OrderInsertDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/OrderInsertDto.java
@@ -1,17 +1,25 @@
 package com.stellaTech.ecommerce.service.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 import java.util.Set;
 
 @Value
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class OrderInsertDto {
+    @EqualsAndHashCode.Include
     Long platformUserId;
+
+    @EqualsAndHashCode.Include
     Set<OrderItemInsertDto> items;
 
     @Value
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
     public static class OrderItemInsertDto {
+        @EqualsAndHashCode.Include
         Long productId;
+
         int productCount;
     }
 }
