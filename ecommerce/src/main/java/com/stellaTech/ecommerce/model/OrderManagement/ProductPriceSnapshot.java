668a142fcbd61e237f805a6e59393ad24803df3a
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-10 01:53:49 -0600

    ProductPriceSnapshot ahora es una entidad de solo lectura

--- ecommerce/src/main/java/com/stellaTech/ecommerce/model/OrderManagement/ProductPriceSnapshot.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/model/OrderManagement/ProductPriceSnapshot.java
@@ -12,10 +12,10 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class ProductPriceSnapshot {
-    @Column(name = "purchased_price", nullable = false, precision = 8, scale = 2)
-    private BigDecimal price;
+    @Column(name = "purchased_price", nullable = false, precision = 8, scale = 2, updatable = false)
+    private BigDecimal price = BigDecimal.ZERO;
 
-    @Column(name = "price_valid_at", nullable = false)
+    @Column(name = "price_valid_at", nullable = false, updatable = false)
     private LocalDateTime validAt;
 
     public ProductPriceSnapshot(BigDecimal currentPrice) {
