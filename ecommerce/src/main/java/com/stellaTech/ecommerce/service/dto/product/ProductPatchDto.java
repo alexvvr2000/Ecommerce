8d287c671f5f7c61cb1d0b356807ce8840d9a5d1
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-09 18:12:38 -0600

    ProductPatchDto implementado

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/product/ProductPatchDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/product/ProductPatchDto.java
@@ -1,4 +1,29 @@
 package com.stellaTech.ecommerce.service.dto.product;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
+@EqualsAndHashCode
 public class ProductPatchDto {
+    String name;
+
+    @Digits(integer = 6, fraction = 2, message = "The price must be 8 digits long; 6 for integers and 2 for decimals")
+    @DecimalMin(value = "0.00", message = "The price must be greater than 0")
+    BigDecimal price;
+
+    String mdFormatDescription;
+
+    String mainImageUrl;
+
+    @Digits(integer = 2, fraction = 2, message = "The rating must be 4 digits long; 2 for integers and 2 for decimals")
+    @DecimalMin(value = "0.00", message = "The rating must be greater than 0")
+    @DecimalMax(value = "10.00", message = "The rating can't be greater than 10")
+    BigDecimal averageRating;
 }
