cd157069a76f156ca0c26908f2a5b9dffed20023
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-13 21:19:01 -0600

    implementados metodos para obtener OrderSelectDto y OrderItemSelectDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/dto/mapper/OrderMapper.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/dto/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.stellaTech.ecommerce.dto.mapper;
 
 import com.stellaTech.ecommerce.dto.order.OrderInsertDto;
+import com.stellaTech.ecommerce.dto.order.OrderSelectDto;
 import com.stellaTech.ecommerce.model.OrderManagement.Order;
 import com.stellaTech.ecommerce.model.OrderManagement.OrderItem;
 import com.stellaTech.ecommerce.model.PlatformUser;
@@ -29,11 +30,28 @@ public abstract class OrderMapper {
     @Mapping(target = "quantity", source = "productCount")
     public abstract OrderItem createOrderItemEntity(OrderInsertDto.OrderItemInsertDto itemDto);
 
+    @Mapping(target = "totalPrice", source = "totalAmount")
+    @Mapping(target = "platformUserId", source = "platformUser")
+    @Mapping(target = "items", source = "orderItems")
+    public abstract OrderSelectDto overviewOfOrderEntity(Order order);
+
+    public OrderSelectDto.OrderItemSelectDto summaryOrderItem(OrderItem orderItem){
+        return new OrderSelectDto.OrderItemSelectDto(
+                orderItem.getProduct().getId(),
+                orderItem.getQuantity(),
+                orderItem.getProductPriceSnapshot().getPrice()
+        );
+    }
+
     protected Product mapProductId(Long productId) {
         return productService.getProductById(productId);
     }
 
     protected PlatformUser mapPlatformUserId(Long platformUserId) {
         return platformUserService.getUserById(platformUserId);
+    }
+
+    protected Long getPlatformUserId(PlatformUser platformUser){
+        return platformUser.getId();
     }
 }
