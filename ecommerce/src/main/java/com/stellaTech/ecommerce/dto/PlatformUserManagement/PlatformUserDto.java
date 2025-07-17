554c406e6164f5f2895455a595b29e1707e19c92
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-17 08:15:30 -0600

    implementado PlatformUserDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/dto/PlatformUserManagement/PlatformUserDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/dto/PlatformUserManagement/PlatformUserDto.java
@@ -1,4 +1,15 @@
 package com.stellaTech.ecommerce.dto.PlatformUserManagement;
 
+import jakarta.validation.constraints.Email;
+import lombok.Builder;
+
+@Builder
 public class PlatformUserDto {
+    private String curp;
+    private String fullName;
+    @Email
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String rfc;
 }
