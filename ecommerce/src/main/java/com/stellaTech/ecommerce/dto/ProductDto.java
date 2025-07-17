6bacdd09b53c9abccdd9b7365503d5b30d90c248
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-17 08:34:49 -0600

    Implementado ProductDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/dto/ProductDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/dto/ProductDto.java
@@ -1,4 +1,15 @@
 package com.stellaTech.ecommerce.dto;
 
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductDto {
+    private String name;
+    private String mdFormatDescription;
+    private String mainImageUrl;
+    @Min(value = 0)
+    private BigDecimal price;
 }
