b32f2ea429faad423d7d538b5ac1b4484e1b5ff8
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-17 13:54:34 -0600

    ahora se usan dtos en OrderController

--- ecommerce/src/main/java/com/stellaTech/ecommerce/restController/OrderController.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/restController/OrderController.java
@@ -1,6 +1,8 @@
 package com.stellaTech.ecommerce.restController;
 
 import com.stellaTech.ecommerce.service.OrderService;
+import com.stellaTech.ecommerce.service.dataDto.OrderDto;
+import com.stellaTech.ecommerce.service.serviceDto.IdDtoResponse;
 import jakarta.validation.Valid;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +19,18 @@ public class OrderController {
     OrderService orderService;
 
     @GetMapping("/orders")
-    public List<OrderSelectDto> getAllOrders() {
+    public List<IdDtoResponse<OrderDto>> getAllOrders() {
         return orderService.getAllOrders();
     }
 
     @GetMapping("/orders/{orderId}")
-    public OrderSelectDto getOrderById(@NonNull @PathVariable Long orderId) {
+    public OrderDto getOrderById(@NonNull @PathVariable Long orderId) {
         return orderService.getOrderDtoById(orderId);
     }
 
     @PostMapping("/orders")
-    public ResponseEntity<?> createOrder(@NonNull @RequestBody @Valid OrderInsertDto orderInsertDto) {
-        OrderSelectDto persistedOrder = orderService.createOrder(orderInsertDto);
+    public ResponseEntity<IdDtoResponse<OrderDto>> createOrder(@NonNull @RequestBody @Valid OrderDto orderInsertDto) {
+        IdDtoResponse<OrderDto> persistedOrder = orderService.createOrder(orderInsertDto);
         return ResponseEntity.ok(persistedOrder);
     }
 
