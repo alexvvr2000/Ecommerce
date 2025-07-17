6366b08e5502d25b0d341b1f7d7177829e4667f5
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-17 08:36:15 -0600

    agregados validaciones ProductDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/dto/OrderDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/dto/OrderDto.java
@@ -1,5 +1,8 @@
 package com.stellaTech.ecommerce.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,12 +11,16 @@ import java.util.List;
 @Data
 public class OrderDto {
     private int platformUserId;
+    @NotNull
+    @NotEmpty
     private List<OrderItemDto> orderItems;
 
     @Data
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
     public static class OrderItemDto {
         @EqualsAndHashCode.Include
         private int productId;
+        @Min(value = 1)
         private int quantity;
     }
 }
