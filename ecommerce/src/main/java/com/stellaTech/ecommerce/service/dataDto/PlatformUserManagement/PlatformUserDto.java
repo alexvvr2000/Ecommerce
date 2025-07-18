08c37fa0385ce9687aa548db9cc06cce19104d49
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 10:28:42 -0600

    eliminado imports innecesarios

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
@@ -1,8 +1,8 @@
 package com.stellaTech.ecommerce.service.dataDto.PlatformUserManagement;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.stellaTech.ecommerce.service.dataDto.validationGroup.NonEmptyCheck;
 import com.stellaTech.ecommerce.service.dataDto.validationGroup.ForbiddenFieldCheck;
+import com.stellaTech.ecommerce.service.dataDto.validationGroup.NonEmptyCheck;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
