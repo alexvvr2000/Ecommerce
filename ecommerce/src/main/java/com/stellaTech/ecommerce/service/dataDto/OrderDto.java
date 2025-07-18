e26ac2af6b8390035e26106414879e9848fe7b41
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 09:32:15 -0600

    agregados genericos para manejo de OrderDto y sus items

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/OrderDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/OrderDto.java
@@ -5,22 +5,57 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Singular;
+import lombok.Value;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class OrderDto {
-    private int platformUserId;
+    private Long platformUserId;
     @NotNull
     @NotEmpty
-    private List<OrderItemDto> orderItems;
+    @Singular
+    private List<OrderItemDto> orderItems = new ArrayList<>();
+
+    public void addItem(OrderItemDto orderInsertDto) {
+        this.orderItems.add(orderInsertDto);
+    }
+
+    public interface OrderItemDto {
+        Long getProductId();
+
+        int getQuantity();
+
+        BigDecimal getPrice();
+    }
 
     @Data
     @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-    public static class OrderItemDto {
+    public static class OrderInsertDto implements OrderItemDto {
         @EqualsAndHashCode.Include
-        private int productId;
+        private Long productId;
         @Min(value = 1)
         private int quantity;
+
+        @Override
+        public BigDecimal getPrice() throws RuntimeException {
+            throw new RuntimeException("The price is set during runtime");
+        }
+    }
+
+    @Value
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    public static class OrderSelectDto implements OrderItemDto {
+        @EqualsAndHashCode.Include
+        Long orderItemId;
+        @EqualsAndHashCode.Include
+        Long orderId;
+        Long productId;
+        int quantity;
+        BigDecimal price;
+        BigDecimal subtotal;
     }
 }
