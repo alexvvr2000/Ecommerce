073b9eed0be9c84063fd8eade7b7a408b3d91b51
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 09:33:42 -0600

    agregados campos @NotNull a PasswordChangeDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PasswordChangeDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PasswordChangeDto.java
@@ -1,10 +1,14 @@
 package com.stellaTech.ecommerce.service.dataDto.PlatformUserManagement;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
 public class PasswordChangeDto {
+    @NotNull
     String newPassword;
+    @NotNull
     String confirmNewPassword;
+    @NotNull
     String oldPassword;
 }
