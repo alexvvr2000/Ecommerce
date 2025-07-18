1292b814f82ec0ff462ed43b1e82fbda6d174dda
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 11:14:28 -0600

    todas las validaciones en grupo ahora van en una sola interfaz

---
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/ValidationGroup.java
@@ -0,0 +1,9 @@
+package com.stellaTech.ecommerce.service.dto;
+
+public interface ValidationGroup {
+    interface OnRead {
+    }
+
+    interface OnChange {
+    }
+}
--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/validationGroup/ForbiddenFieldChangeCheck.java
+++
@@ -1,4 +0,0 @@
-package com.stellaTech.ecommerce.service.dto.validationGroup;
-
-public interface ForbiddenFieldChangeCheck {
-}
