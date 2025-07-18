5f5d9f6dd5cc4a4d0ce839c95b8f002fe3c312a2
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 12:20:03 -0600

    agregado mappers para intercambios normales y cuando son patch

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/MapperConfig.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/MapperConfig.java
@@ -1,4 +1,20 @@
 package com.stellaTech.ecommerce.service.dto;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MapperConfig {
+    @Bean
+    public ModelMapper defaultMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ModelMapper patchMapper() {
+        ModelMapper mapper = defaultMapper();
+        mapper.getConfiguration().setSkipNullEnabled(true);
+        return mapper;
+    }
 }
