12947c77b8bab78050a47d09302c3265285862c2
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 09:34:37 -0600

    agregadas anotaciones a PlatformUserDto para prevenir lectura de contraseñas

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
@@ -1,17 +1,21 @@
 package com.stellaTech.ecommerce.service.dataDto.PlatformUserManagement;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Email;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlatformUserDto {
     private String curp;
     private String fullName;
     @Email
     private String email;
     private String phoneNumber;
+    @JsonIgnore
     private String password;
     private String rfc;
 }
