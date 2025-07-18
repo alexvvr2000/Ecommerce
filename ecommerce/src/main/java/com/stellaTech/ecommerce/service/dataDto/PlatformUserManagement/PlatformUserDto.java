912592e4df891d7dcd4987f7529e99ce7f41dad7
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-18 09:45:51 -0600

    agregados chequeos para cada grupo de PlatformUserDto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dataDto/PlatformUserManagement/PlatformUserDto.java
@@ -1,7 +1,11 @@
 package com.stellaTech.ecommerce.service.dataDto.PlatformUserManagement;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.stellaTech.ecommerce.service.dataDto.validationGroup.InsertUpdateCheck;
+import com.stellaTech.ecommerce.service.dataDto.validationGroup.PatchCheck;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +14,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlatformUserDto {
+    @NotNull(groups = InsertUpdateCheck.class)
     private String curp;
+    @NotNull(groups = InsertUpdateCheck.class)
     private String fullName;
+    @NotNull(groups = InsertUpdateCheck.class)
     @Email
     private String email;
     private String phoneNumber;
+    @Null(groups = PatchCheck.class)
+    @NotNull(groups = InsertUpdateCheck.class)
     @JsonIgnore
     private String password;
+    @NotNull(groups = InsertUpdateCheck.class)
     private String rfc;
 }
