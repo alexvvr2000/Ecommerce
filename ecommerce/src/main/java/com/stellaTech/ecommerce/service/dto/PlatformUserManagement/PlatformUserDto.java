1336fe91442cb01423ac426a4813c187dc9ce4b3
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-07-19 08:13:28 -0600

    ahora es explicito cuando se quiere la contraseña en dto

--- ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/PlatformUserManagement/PlatformUserDto.java
+++ ecommerce/src/main/java/com/stellaTech/ecommerce/service/dto/PlatformUserManagement/PlatformUserDto.java
@@ -1,6 +1,5 @@
 package com.stellaTech.ecommerce.service.dto.PlatformUserManagement;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.stellaTech.ecommerce.service.dto.ValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -13,25 +12,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlatformUserDto {
-    @Null(groups = ValidationGroup.OnInsert.class)
+    @Null(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnUpdate.class})
     @NotNull(groups = ValidationGroup.OnRead.class)
     private Long id;
 
-    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class})
+    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class, ValidationGroup.OnUpdate.class})
     private String curp;
 
-    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class})
+    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class, ValidationGroup.OnUpdate.class})
     private String fullName;
 
-    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class})
+    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class, ValidationGroup.OnUpdate.class})
     @Email
     private String email;
 
-    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class})
+    @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class, ValidationGroup.OnUpdate.class})
     private String phoneNumber;
 
+    @Null(groups = ValidationGroup.OnUpdate.class)
     @NotNull(groups = {ValidationGroup.OnInsert.class, ValidationGroup.OnRead.class})
-    @JsonIgnore
     private String password;
 
     private String rfc;
