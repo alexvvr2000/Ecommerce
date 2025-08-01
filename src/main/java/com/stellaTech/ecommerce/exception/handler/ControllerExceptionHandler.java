2b201d95d550ed55d5701f478936e84e72e0e797
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-01 11:38:55 -0600

    implementados mensajes de error en ControllerExceptionHandler

--- src/main/java/com/stellaTech/ecommerce/exception/handler/ControllerExceptionHandler.java
+++ src/main/java/com/stellaTech/ecommerce/exception/handler/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.stellaTech.ecommerce.exception.handler;
 
 import com.stellaTech.ecommerce.exception.handler.ErrorMessage.ErrorMessage;
+import com.stellaTech.ecommerce.exception.handler.ErrorMessage.InvalidObjectErrorMessage;
 import com.stellaTech.ecommerce.exception.instance.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,32 +15,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDate;
-import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(value = {ResourceNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException exception, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
-                exception.getClass().getName(),
-                LocalDate.now(),
-                exception.getMessage()
-        );
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(
+            ResourceNotFoundException exception, WebRequest request
+    ) {
+        ErrorMessage message = ErrorMessage.builder()
+                .errorName(exception.getClass().getName())
+                .date(LocalDate.now())
+                .message(exception.getMessage())
+                .build();
+        ResponseEntity<ErrorMessage> responseEntity = new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        log.error(responseEntity.toString());
+        return responseEntity;
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<ErrorMessage> notValidArgumentException(MethodArgumentNotValidException exception, WebRequest request) {
-        List<String> listOfErrors = exception.getBindingResult().getAllErrors().stream().map(
-                objectError -> ((FieldError) objectError).getField()
-        ).toList();
-        ErrorMessage message = new ErrorMessage(
-                exception.getClass().getName(),
-                LocalDate.now(),
-                "Invalid fields: " + listOfErrors
+    public ResponseEntity<InvalidObjectErrorMessage> notValidArgumentException(
+            MethodArgumentNotValidException exception, WebRequest request
+    ) {
+        InvalidObjectErrorMessage.InvalidObjectErrorMessageBuilder message = InvalidObjectErrorMessage.builder()
+                .errorName(exception.getClass().getName())
+                .message("Many fields in the request were not valid")
+                .date(LocalDate.now());
+        for (ObjectError error : exception.getBindingResult().getAllErrors()) {
+            FieldError fieldErrorData = ((FieldError) error);
+            InvalidObjectErrorMessage.InvalidFieldMessage<Object> fieldErrorEntry = InvalidObjectErrorMessage
+                    .InvalidFieldMessage
+                    .builder()
+                    .fieldName(fieldErrorData.getField())
+                    .Message(fieldErrorData.getDefaultMessage())
+                    .rejectedValue(fieldErrorData.getRejectedValue())
+                    .build();
+            message.invalidField(fieldErrorEntry);
+        }
+        ResponseEntity<InvalidObjectErrorMessage> responseEntity = new ResponseEntity<>(
+                message.build(), HttpStatus.NOT_ACCEPTABLE
         );
-        return new ResponseEntity<>(message, HttpStatus.NOT_ACCEPTABLE);
+        log.error(responseEntity.toString());
+        return responseEntity;
     }
 }
