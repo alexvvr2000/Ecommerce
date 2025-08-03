5bca89782f8adfad4217a3086e2e95aa72320b9d
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-02 21:54:47 -0600

    Arreglado status Http en excepciones

--- src/main/java/com/stellaTech/ecommerce/exception/instance/InvalidPasswordField.java
+++ src/main/java/com/stellaTech/ecommerce/exception/instance/InvalidPasswordField.java
@@ -3,7 +3,7 @@ package com.stellaTech.ecommerce.exception.instance;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidPasswordField extends IllegalArgumentException {
     public InvalidPasswordField(String message) {
         super(message);
--- src/main/java/com/stellaTech/ecommerce/exception/instance/InvalidProductQuantity.java
+++ src/main/java/com/stellaTech/ecommerce/exception/instance/InvalidProductQuantity.java
@@ -3,7 +3,7 @@ package com.stellaTech.ecommerce.exception.instance;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidProductQuantity extends RuntimeException {
     public InvalidProductQuantity(String message) {
         super(message);
