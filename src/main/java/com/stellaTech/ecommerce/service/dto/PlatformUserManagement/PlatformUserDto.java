5e51cbdf9cc63f6ca3f9f0be8d71058a3f67c212
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-02 22:06:38 -0600

    Arreglado chequeo de nulos en PlatformUserDto

--- src/main/java/com/stellaTech/ecommerce/service/dto/PlatformUserManagement/PlatformUserDto.java
+++ src/main/java/com/stellaTech/ecommerce/service/dto/PlatformUserManagement/PlatformUserDto.java
@@ -2,8 +2,10 @@ package com.stellaTech.ecommerce.service.dto.PlatformUserManagement;
 
 import com.stellaTech.ecommerce.service.dto.NullCheckGroup;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,21 +21,21 @@ public class PlatformUserDto {
     @NotNull(groups = NullCheckGroup.OnRead.class)
     private Long id;
 
-    @NotNull(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnRead.class, NullCheckGroup.OnUpdate.class})
+    @NotBlank(groups = {NullCheckGroup.OnInsert.class ,NullCheckGroup.OnUpdate.class})
     private String curp;
 
-    @NotNull(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnRead.class, NullCheckGroup.OnUpdate.class})
+    @NotBlank(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnUpdate.class})
     private String fullName;
 
-    @NotNull(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnRead.class, NullCheckGroup.OnUpdate.class})
+    @NotBlank(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnUpdate.class})
     @Email
     private String email;
 
-    @NotNull(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnRead.class, NullCheckGroup.OnUpdate.class})
+    @NotBlank(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnUpdate.class})
     private String phoneNumber;
 
-    @Null(groups = {NullCheckGroup.OnUpdate.class, NullCheckGroup.OnRead.class})
-    @NotNull(groups = NullCheckGroup.OnInsert.class)
+    @Null(groups = {NullCheckGroup.OnRead.class})
+    @NotBlank(groups = {NullCheckGroup.OnInsert.class, NullCheckGroup.OnUpdate.class})
     private String password;
 
     private String rfc;
