f2f6b22d07c278958c48a7a8f2ad342d9471b291
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-02 19:41:51 -0600

    agregadas excepciones a ControllerExceptionHandler

--- src/main/java/com/stellaTech/ecommerce/exception/handler/ControllerExceptionHandler.java
+++ src/main/java/com/stellaTech/ecommerce/exception/handler/ControllerExceptionHandler.java
@@ -2,6 +2,9 @@ package com.stellaTech.ecommerce.exception.handler;
 
 import com.stellaTech.ecommerce.exception.handler.ErrorMessage.ErrorMessage;
 import com.stellaTech.ecommerce.exception.handler.ErrorMessage.InvalidObjectErrorMessage;
+import com.stellaTech.ecommerce.exception.instance.InvalidPasswordField;
+import com.stellaTech.ecommerce.exception.instance.InvalidProductQuantity;
+import com.stellaTech.ecommerce.exception.instance.RepeatedUserPassword;
 import com.stellaTech.ecommerce.exception.instance.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,19 +22,47 @@ import java.time.LocalDate;
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler(value = {ResourceNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorMessage> resourceNotFoundException(
-            ResourceNotFoundException exception, WebRequest request
-    ) {
+    private ResponseEntity<ErrorMessage> buildGenericMessage(Exception exception, HttpStatus status) {
         ErrorMessage message = ErrorMessage.builder()
                 .errorName(exception.getClass().getName())
                 .date(LocalDate.now())
                 .message(exception.getMessage())
                 .build();
-        ResponseEntity<ErrorMessage> responseEntity = new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        ResponseEntity<ErrorMessage> responseEntity = new ResponseEntity<>(message, HttpStatus.NOT_ACCEPTABLE);
         log.error(responseEntity.toString());
         return responseEntity;
+    }
+
+    @ExceptionHandler(value = InvalidPasswordField.class)
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public ResponseEntity<ErrorMessage> invalidPasswordFieldException(
+            InvalidPasswordField exception, WebRequest request
+    ) {
+        return buildGenericMessage(exception, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(value = InvalidProductQuantity.class)
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public ResponseEntity<ErrorMessage> invalidPasswordFieldException(
+            InvalidProductQuantity exception, WebRequest request
+    ) {
+        return buildGenericMessage(exception, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(value = RepeatedUserPassword.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorMessage> repeatedUserPassword(
+            RepeatedUserPassword exception, WebRequest request
+    ) {
+        return buildGenericMessage(exception, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = {ResourceNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(
+            ResourceNotFoundException exception, WebRequest request
+    ) {
+        return buildGenericMessage(exception, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
