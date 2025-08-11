8c09e417c2ed2c9b0b6340abb33db885208347cf
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-10 23:36:21 -0600

    ahora DataGenerationService es una clase que no depende la base de datos

--- src/test/java/com/stellaTech/ecommerce/DataGenerationService.java
+++ src/test/java/com/stellaTech/ecommerce/DataGenerationService.java
@@ -5,16 +5,10 @@ import com.stellaTech.ecommerce.service.dto.OrderDto;
 import com.stellaTech.ecommerce.service.dto.ProductDto;
 import com.stellaTech.ecommerce.service.dto.checkGroup.NullCheckGroup;
 import com.stellaTech.ecommerce.service.dto.platformUserManagement.PlatformUserDto;
-import com.stellaTech.ecommerce.service.generics.OrderService;
-import com.stellaTech.ecommerce.service.generics.PlatformUserService;
-import com.stellaTech.ecommerce.service.generics.ProductService;
 import jakarta.validation.Valid;
 import lombok.Builder;
 import lombok.Value;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
@@ -22,20 +16,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-@Transactional
-@Rollback
 @Service
 public class DataGenerationService {
     protected final Faker faker = new Faker(new Locale("es-MX"));
-
-    @Autowired
-    protected OrderService orderServiceImp;
-
-    @Autowired
-    protected PlatformUserService platformUserServiceImp;
-
-    @Autowired
-    protected ProductService productServiceImp;
 
     public @Validated(NullCheckGroup.OnInsert.class) PlatformUserDto createInsertUserDto() {
         return PlatformUserDto.builder()
@@ -47,11 +30,6 @@ public class DataGenerationService {
                 .build();
     }
 
-    public @Validated(NullCheckGroup.OnRead.class) PlatformUserDto createPersistedUser() {
-        PlatformUserDto newUser = createInsertUserDto();
-        return platformUserServiceImp.createUser(newUser);
-    }
-
     public @Validated(NullCheckGroup.OnInsert.class) ProductDto createInsertProductDto() {
         return ProductDto.builder()
                 .name(faker.commerce().productName())
@@ -59,11 +37,6 @@ public class DataGenerationService {
                 .mainImageUrl(faker.internet().image())
                 .price(new BigDecimal(faker.commerce().price().replace(",", ".")))
                 .build();
-    }
-
-    public @Validated(NullCheckGroup.OnRead.class) ProductDto createPersistedProduct() {
-        ProductDto newProduct = createInsertProductDto();
-        return productServiceImp.createProduct(newProduct);
     }
 
     public @Valid OrderDto.OrderItemInsertDto createInsertDtoOrderItem(
@@ -76,26 +49,12 @@ public class DataGenerationService {
     }
 
     public @Valid OrderDto<OrderDto.OrderItemInsertDto> createInsertDtoOrder(
-            int itemAmount,
-            @Validated(NullCheckGroup.OnRead.class) PlatformUserDto user,
-            numberRange itemAmountRange
-    ) {
-        List<OrderDto.OrderItemInsertDto> productList = createInsertDtoOrderItemList(
-                itemAmount, itemAmountRange
-        );
-        return OrderDto
-                .<OrderDto.OrderItemInsertDto>builder()
-                .platformUserId(user.getId())
-                .orderItems(productList)
-                .build();
-    }
-
-    public @Valid List<OrderDto.OrderItemInsertDto> createInsertDtoOrderItemList(
-            int itemAmount, numberRange itemAmountRange
+            @Validated(NullCheckGroup.OnRead.class) List<ProductDto> productDtoList,
+            numberRange itemAmountRange,
+            @Validated(NullCheckGroup.OnRead.class) PlatformUserDto user
     ) {
         List<OrderDto.OrderItemInsertDto> orderItems = new ArrayList<>();
-        for (int i = 0; i < itemAmount; i += 1) {
-            ProductDto newProduct = createPersistedProduct();
+        for (ProductDto newProduct : productDtoList) {
             int orderItemAmount = faker.number().numberBetween(
                     itemAmountRange.minAmount, itemAmountRange.maxAmount
             );
@@ -103,18 +62,11 @@ public class DataGenerationService {
                     createInsertDtoOrderItem(newProduct, orderItemAmount)
             );
         }
-        return orderItems;
-    }
-
-    public @Valid OrderDto<OrderDto.OrderItemSelectDto> createPersistedOrder(
-            int itemAmount,
-            @Validated(NullCheckGroup.OnRead.class) PlatformUserDto user,
-            numberRange itemSelectionRange
-    ) {
-        OrderDto<OrderDto.OrderItemInsertDto> newOrder = createInsertDtoOrder(
-                itemAmount, user, itemSelectionRange
-        );
-        return orderServiceImp.createOrder(newOrder);
+        return OrderDto
+                .<OrderDto.OrderItemInsertDto>builder()
+                .platformUserId(user.getId())
+                .orderItems(orderItems)
+                .build();
     }
 
     @Value
