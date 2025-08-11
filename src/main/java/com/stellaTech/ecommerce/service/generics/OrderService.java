6d93b19e559b55e5b149a54d7123680330e10577
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-10 22:52:50 -0600

    eliminada validacion de jakarta de las interfaces de los servicios

--- src/main/java/com/stellaTech/ecommerce/service/generics/OrderService.java
+++ src/main/java/com/stellaTech/ecommerce/service/generics/OrderService.java
@@ -2,18 +2,15 @@ package com.stellaTech.ecommerce.service.generics;
 
 import com.stellaTech.ecommerce.exception.instance.ResourceNotFoundException;
 import com.stellaTech.ecommerce.service.dto.OrderDto;
-import com.stellaTech.ecommerce.service.dto.checkGroup.NullCheckGroup;
-import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.validation.annotation.Validated;
 
 public interface OrderService extends LogicallyDeletableEntityService {
     OrderDto<OrderDto.OrderItemSelectDto> createOrder(
-            @Validated(NullCheckGroup.OnInsert.class) OrderDto<OrderDto.OrderItemInsertDto> dto
+            OrderDto<OrderDto.OrderItemInsertDto> dto
     ) throws ResourceNotFoundException;
 
-    Page<OrderDto<OrderDto.OrderItemSelectDto>> getAllOrders(@NonNull Pageable pageable);
+    Page<OrderDto<OrderDto.OrderItemSelectDto>> getAllOrders(Pageable pageable);
 
     OrderDto<OrderDto.OrderItemSelectDto> getOrderDtoById(Long id) throws ResourceNotFoundException;
 }
--- src/main/java/com/stellaTech/ecommerce/service/generics/ProductService.java
+++ src/main/java/com/stellaTech/ecommerce/service/generics/ProductService.java
@@ -2,21 +2,17 @@ package com.stellaTech.ecommerce.service.generics;
 
 import com.stellaTech.ecommerce.exception.instance.ResourceNotFoundException;
 import com.stellaTech.ecommerce.service.dto.ProductDto;
-import com.stellaTech.ecommerce.service.dto.checkGroup.NullCheckGroup;
-import jakarta.validation.Valid;
-import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.validation.annotation.Validated;
 
 public interface ProductService extends LogicallyDeletableEntityService {
-    ProductDto updateProduct(Long productId, @Validated(NullCheckGroup.OnUpdate.class) ProductDto dto) throws ResourceNotFoundException;
+    ProductDto updateProduct(Long productId, ProductDto dto) throws ResourceNotFoundException;
 
-    ProductDto patchProduct(Long id, @Valid ProductDto dto) throws ResourceNotFoundException;
+    ProductDto patchProduct(Long id, ProductDto dto) throws ResourceNotFoundException;
 
-    ProductDto createProduct(@Validated(NullCheckGroup.OnInsert.class) ProductDto dto);
+    ProductDto createProduct(ProductDto dto);
 
-    Page<ProductDto> getAllProductsPaginated(@NonNull Pageable pageable);
+    Page<ProductDto> getAllProductsPaginated(Pageable pageable);
 
     ProductDto getProductDtoById(Long id) throws ResourceNotFoundException;
 }
