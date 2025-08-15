56f9c5e5f8d51ab5657b52fb2a45ba5f589d2e5a
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-14 19:00:53 -0600

    ahora creacion de ordenes son por medio de servicio de datos

--- src/test/java/com/stellaTech/ecommerce/service/implementation/OrderServiceIntegrationTests.java
+++ src/test/java/com/stellaTech/ecommerce/service/implementation/OrderServiceIntegrationTests.java
@@ -1,6 +1,5 @@
 package com.stellaTech.ecommerce.service.implementation;
 
-import com.github.javafaker.Faker;
 import com.stellaTech.ecommerce.DataGenerationService;
 import com.stellaTech.ecommerce.exception.instance.ResourceNotFoundException;
 import com.stellaTech.ecommerce.service.dto.OrderDto;
@@ -13,14 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,55 +23,43 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class OrderServiceIntegrationTests {
-    protected final Faker faker = new Faker(new Locale("es-MX"));
-    protected final DataGenerationService.numberRange productCreationRange = DataGenerationService.numberRange.builder()
+    protected final DataGenerationService.NumberRange orderItemAmountRange = DataGenerationService.NumberRange.builder()
             .minAmount(1)
             .maxAmount(10)
             .build();
-    protected final DataGenerationService.numberRange orderItemAmountRange = DataGenerationService.numberRange.builder()
-            .minAmount(1)
-            .maxAmount(10)
-            .build();
-    protected final int orderCreationLimit = 10;
-    protected final int randomItemAmount = faker.number().numberBetween(
-            productCreationRange.getMinAmount(), productCreationRange.getMaxAmount()
-    );
-    protected final Pageable defaultPageableObject = Pageable.ofSize(orderCreationLimit);
+    protected final Long testOrderId = 1L;
+    protected final Long testUserId = 1L;
+    protected final int amountListProductDto = 7;
     @Autowired
     protected DataGenerationService dataGenerationService;
     @Autowired
     protected OrderService orderService;
     @Autowired
-    protected ProductService productService;
-    @Autowired
     protected PlatformUserService platformUserService;
+    @Autowired
+    protected ProductService productService;
 
-    private List<ProductDto> generateRandomProductList(int itemAmount) {
-        List<ProductDto> list = new ArrayList<>();
-        for (int i = 0; i < itemAmount; i += 1) {
-            ProductDto newProduct = productService.createProduct(
-                    dataGenerationService.createInsertProductDto()
-            );
-            list.add(
-                    newProduct
-            );
-        }
-        return list;
-    }
-
-    private OrderDto<OrderDto.OrderItemSelectDto> createPersistedOrder() {
-        PlatformUserDto randomUser = platformUserService.createUser(
-                dataGenerationService.createInsertUserDto()
+    private OrderDto<OrderDto.OrderItemSelectDto> createPersistedOrder(PlatformUserDto userDto, Long orderId, DataGenerationService.NumberRange productListSizeRange) {
+        List<ProductDto> productDtoList = dataGenerationService.createValidListProductDto(
+                amountListProductDto, productListSizeRange
         );
-        OrderDto<OrderDto.OrderItemInsertDto> newOrder = dataGenerationService.createInsertDtoOrder(
-                generateRandomProductList(randomItemAmount), orderItemAmountRange, randomUser
+        OrderDto<OrderDto.OrderItemInsertDto> newOrder = dataGenerationService.createValidOrderDto(
+                productDtoList.stream().map(
+                        productDto -> productService.createProduct(productDto)
+                ).toList(),
+                orderItemAmountRange, userDto, orderId
         );
         return orderService.createOrder(newOrder);
     }
 
     @Test
     void createOrderWithValidData() {
-        OrderDto<OrderDto.OrderItemSelectDto> newOrder = createPersistedOrder();
+        PlatformUserDto platformUserDto =  dataGenerationService.createValidPlatformUserDto(testUserId);
+        OrderDto<OrderDto.OrderItemSelectDto> newOrder = createPersistedOrder(
+                platformUserService.createUser(platformUserDto),
+                testOrderId,
+                orderItemAmountRange
+        );
         OrderDto<OrderDto.OrderItemSelectDto> savedOrder = orderService.getOrderDtoById(
                 newOrder.getId()
         );
@@ -100,7 +83,12 @@ public class OrderServiceIntegrationTests {
 
     @Test
     void getOrderDtoByIdWhenOrderExists() {
-        OrderDto<OrderDto.OrderItemSelectDto> savedOrder = createPersistedOrder();
+        PlatformUserDto userDto = dataGenerationService.createValidPlatformUserDto(testUserId);
+        OrderDto<OrderDto.OrderItemSelectDto> savedOrder = createPersistedOrder(
+                platformUserService.createUser(userDto),
+                testOrderId,
+                orderItemAmountRange
+        );
         assertNotNull(savedOrder);
         assertEquals(
                 orderService.getOrderDtoById(savedOrder.getId()),
@@ -110,22 +98,17 @@ public class OrderServiceIntegrationTests {
 
     @Test
     void deleteOrderFromDatabase() {
-        OrderDto<OrderDto.OrderItemSelectDto> persistedOrderToDelete = createPersistedOrder();
+        PlatformUserDto userDto = dataGenerationService.createValidPlatformUserDto(testUserId);
+        OrderDto<OrderDto.OrderItemSelectDto> persistedOrderToDelete = createPersistedOrder(
+                platformUserService.createUser(userDto),
+                testOrderId,
+                orderItemAmountRange
+        );
         Long persistedOrderId = persistedOrderToDelete.getId();
         assertEquals(orderService.getOrderDtoById(persistedOrderId), persistedOrderToDelete);
-
-        for (int i = 0; i < orderCreationLimit; i += 1) {
-            createPersistedOrder();
-        }
 
         orderService.logicallyDeleteById(persistedOrderToDelete.getId());
         assertThrows(ResourceNotFoundException.class, () -> orderService.logicallyDeleteById(persistedOrderId));
         assertThrows(ResourceNotFoundException.class, () -> orderService.getOrderDtoById(persistedOrderId));
-
-        Page<OrderDto<OrderDto.OrderItemSelectDto>> orderSelectionPage = orderService.getAllOrders(
-                defaultPageableObject
-        );
-        boolean deletedOrderExistsInPage = orderSelectionPage.getContent().contains(persistedOrderToDelete);
-        assertFalse(deletedOrderExistsInPage);
     }
 }
