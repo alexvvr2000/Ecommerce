8ba2502fec681e5771c049ffa0e813b0bcaab63f
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2025-08-14 22:16:54 -0600

    agregados unit tests para OrderServiceUnitTest

---
+++ src/test/java/com/stellaTech/ecommerce/service/implementation/OrderServiceUnitTest.java
@@ -0,0 +1,113 @@
+package com.stellaTech.ecommerce.service.implementation;
+
+import com.stellaTech.ecommerce.DataGenerationService;
+import com.stellaTech.ecommerce.model.orderManagement.CustomerOrder;
+import com.stellaTech.ecommerce.repository.OrderRepository;
+import com.stellaTech.ecommerce.repository.PlatformUserRepository;
+import com.stellaTech.ecommerce.repository.ProductRepository;
+import com.stellaTech.ecommerce.service.dto.OrderDto;
+import com.stellaTech.ecommerce.service.dto.ProductDto;
+import com.stellaTech.ecommerce.service.dto.platformUserManagement.PlatformUserDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceUnitTest {
+    protected final DataGenerationService.NumberRange orderItemQuantityRange =
+            DataGenerationService.NumberRange.builder()
+                    .minAmount(1)
+                    .maxAmount(10)
+                    .build();
+    protected final DataGenerationService.NumberRange orderItemCreationRange =
+            DataGenerationService.NumberRange.builder()
+                    .minAmount(1)
+                    .maxAmount(10)
+                    .build();
+    protected final DataGenerationService.NumberRange idCreationRange =
+            DataGenerationService.NumberRange.builder()
+                    .minAmount(1)
+                    .maxAmount(10)
+                    .build();
+    protected final Long testOrderId = 1L;
+    protected final Long testUserId = 1L;
+    protected final int orderItemListSize = 7;
+    protected final DataGenerationService dataGenerationService = new DataGenerationService();
+    @Mock
+    protected ProductRepository productRepository;
+    @Mock
+    protected PlatformUserRepository userRepository;
+    @Mock
+    protected OrderRepository orderRepository;
+    @InjectMocks
+    protected OrderServiceImp orderService;
+
+    @Test
+    void createOrderWithValidData() throws NoSuchFieldException, IllegalAccessException {
+        List<ProductDto> productDtoList = dataGenerationService.createValidListProductDto(
+                orderItemListSize, idCreationRange
+        );
+        productDtoList.forEach(
+                currentDto -> {
+                    try {
+                        when(productRepository.getProductById(currentDto.getId()))
+                                .thenReturn(dataGenerationService.createProductModel(currentDto));
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+
+        PlatformUserDto userData = dataGenerationService.createValidPlatformUserDto(testUserId);
+        when(userRepository.getUserById(testUserId)).thenReturn(
+                dataGenerationService.createPlatformUserModel(userData)
+        );
+
+        OrderDto<OrderDto.OrderItemInsertDto> validOrderDto = dataGenerationService.createValidOrderDto(
+                productDtoList, orderItemQuantityRange, userData.getId(), testOrderId
+        );
+        OrderDto<OrderDto.OrderItemSelectDto> result = orderService.createOrder(validOrderDto);
+        assertEquals(result.getOrderItems().size(), productDtoList.size());
+    }
+
+    @Test
+    void getOrderIdWhenExistsInDatabase() throws NoSuchFieldException, IllegalAccessException {
+        List<ProductDto> productDtoList = dataGenerationService.createValidListProductDto(
+                orderItemListSize, idCreationRange
+        );
+        OrderDto<OrderDto.OrderItemInsertDto> validOrderDto = dataGenerationService.createValidOrderDto(
+                productDtoList, orderItemQuantityRange, testUserId, testOrderId
+        );
+
+        CustomerOrder mockOrder = dataGenerationService.createCustomerOrderModel(validOrderDto);
+        when(orderRepository.getOrderById(testOrderId)).thenReturn(mockOrder);
+
+        OrderDto<OrderDto.OrderItemSelectDto> result = orderService.getOrderDtoById(testOrderId);
+        assertNotNull(result);
+        assertEquals(result.getOrderItems().size(), productDtoList.size());
+    }
+
+    @Test
+    void logicallyDeleteByIdWhenOrderExists() throws NoSuchFieldException, IllegalAccessException {
+        OrderDto<OrderDto.OrderItemInsertDto> newOrder = dataGenerationService.createValidOrderDto(
+                dataGenerationService.createValidListProductDto(orderItemListSize, idCreationRange),
+                orderItemCreationRange,
+                testUserId,
+                testOrderId
+        );
+
+        CustomerOrder mockOrder = dataGenerationService.createCustomerOrderModel(newOrder);
+        when(orderRepository.getOrderById(testOrderId)).thenReturn(mockOrder);
+
+        assertFalse(mockOrder.isDeleted());
+        orderService.logicallyDeleteById(testOrderId);
+        assertTrue(mockOrder.isDeleted());
+    }
+}
